108cf5b5c62cd4198247772643599dd8f8476c35
Wonil Heo <wonza8585@gmail.com>
2023-06-23 21:18:08 +0900

    websocket을 이용한 채팅기능

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,18 +1,43 @@
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 // socket: 서버로의 연결(브라우저는 백엔드와의 연결을 제공)
 const socket = new WebSocket(`ws://${window.location.host}`);
 
+// 새로운 메시지 생성: 객체 생성 -> JSON 타입으로 변환 -> 백엔드로 전송
+function makeMessage(type, payload) {
+    const msg = { type, payload };
+    return JSON.stringify(msg);
+}
 socket.addEventListener("open", () => {
     console.log("Connected to Server ✔");
 });
 
 socket.addEventListener("message", (message) => {
-    console.log("New message: ", message.data);
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
     console.log("Disconnected from Server ❌");
 })
 
-setTimeout(() => {
-    socket.send("hello from the browser");
-}, 10000);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value));
+    const li = document.createElement("li");
+    li.innerText = `You: ${input.value}`;
+    messageList.append(li);
+    input.value = "";
+}
+
+function handleNickSubmit(event) {
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
+}
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -10,7 +10,6 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => conso le.log(`Listening on http://localhost:3000`);
 
 // 동일 서버에서 http, webSocket을 둘 다 작동시킴
 // express.js를 이용하여 http 서버 구동
@@ -18,16 +17,31 @@ const server = http.createServer(app);
 // http 서버 위 webSocket 서버 구동
 const wss = new WebSocket.Server({ server });
 
-// socket: 연결된 브라우저 => 내부 메서드들은 socket을 위한 (wss는 서버 전체를 위한 객체)
+
+
+const sockets = [];
+
+// socket: 연결된 브라우저 => 내부 메서드들은 socket의 메서드 (wss는 서버 전체를 위한 객체)
 wss.on("connection", (socket) => {
     console.log("Connected to Browser ✔");
+    sockets.push(socket);
+    socket["nickname"] = "Anon";
     socket.on("close", () => {
         console.log("Disconnected from Browser ❌");
     });
-    socket.on("message", message => {
-        console.log(message.toString('utf8'));
-    });
-    socket.send("hello!!!");
+    socket.on("message", msg => {
+        const message = JSON.parse(msg);
+        switch (message.type) {
+            case "new_message":
+                sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));
+                break;
+            case "nickname":
+                socket["nickname"] = message.payload;
+                break
+            }
+        });
 });
 
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
 server.listen(3000, handleListen);
+
