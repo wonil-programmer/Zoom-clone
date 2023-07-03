2f6c506fabe067aefae07d97438467ef03e4e0a8
원일 허 <70367717+wonil-programmer@users.noreply.github.com>
2023-07-03 19:47:04 +0900

    IceCandidate 및 RTC 구현

--- src/serverVideo.js
+++ src/serverVideo.js
@@ -16,12 +16,17 @@ const wsServer = SocketIO(httpServer);
 wsServer.on("connection", socket => {
     socket.on("join_room", (roomName, done) => {
         socket.join(roomName);
-        done();
         socket.to(roomName).emit("welcome");
     })
     socket.on("offer", (offer, roomName) => {
         socket.to(roomName).emit("offer", offer);
     });
+    socket.on("answer", (answer, roomName) => {
+        socket.to(roomName).emit("answer", answer); 
+    })
+    socket.on("ice", (ice, roomName) => {
+        socket.to(roomName).emit("ice", ice);
+    })
 });
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
