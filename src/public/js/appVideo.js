034fe58bb6e5999d3ce75ed32c7346efff2df13f
원일 허 <70367717+wonil-programmer@users.noreply.github.com>
2023-07-03 12:41:01 +0900

    welcome form ~ RTC initialize

--- src/public/js/appVideo.js
+++ src/public/js/appVideo.js
@@ -5,9 +5,15 @@ const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras");
 
+const call = document.getElementById("call");
+
+call.hidden = true;
+
 let myStream;
 let muted = false;
 let cameraOff = false;
+let roomName;
+let myPeerConnection;
 
 async function getCameras() {
   try {
@@ -39,21 +45,20 @@ async function getMedia(deviceId) {
   try {
     myStream = await navigator.mediaDevices.getUserMedia(
       deviceId ? cameraConstraints : initialConstraints
-    );
-    myFace.srcObject = myStream;
-    if (!deviceId) {
-      await getCameras();
+      );
+      myFace.srcObject = myStream;
+      if (!deviceId) {
+        await getCameras();
+      }
+    } catch (e) {
+      console.log(e);
     }
-  } catch (e) {
-    console.log(e);
   }
-}
-getMedia();
-
-function handleMuteClick() {
+  
+  function handleMuteClick() {
     myStream
-      .getAudioTracks()
-      .forEach((track) => (track.enabled = !track.enabled));
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
     if (!muted) {
       muteBtn.innerText = "Unmute";
       muted = true;
@@ -61,12 +66,12 @@ function handleMuteClick() {
       muteBtn.innerText = "Mute";
       muted = false;
     }
-}
-
-function handleCameraClick() {
+  }
+  
+  function handleCameraClick() {
     myStream
-      .getVideoTracks()
-      .forEach((track) => (track.enabled = !track.enabled));
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
     if (cameraOff) {
       cameraBtn.innerText = "Turn Camera Off";
       cameraOff = false;
@@ -79,7 +84,50 @@ function handleCameraClick() {
 async function handleCameraChange() {
   await getMedia(camerasSelect.value);
 }
-  
+
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
 camerasSelect.addEventListener("input", handleCameraChange);
+
+// Welcome Form (join a room)
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form");
+
+async function startMedia() {
+  welcome.hidden = true;
+  call.hidden = false;
+  await getMedia();
+  makeConnection();
+}
+
+function handleWelcomeSubmit(event) {
+  event.preventDefault();
+  const input = welcomeForm.querySelector("input");
+  socket.emit("join_room", input.value, startMedia);
+  roomName = input.value;
+  input.value = "";
+}
+
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+
+// Socket Code
+
+socket.on("welcome", async () => {
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  console.log("sent the offer");
+  socket.emit("offer", offer, roomName);
+});
+
+socket.on("offer", offer => {
+  console.log(offer);
+})
+
+// RTC Code
+
+function makeConnection() {
+  myPeerConnection = new RTCPeerConnection();
+  myStream
+    .getTracks()
+    .forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
